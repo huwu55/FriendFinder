4a1f192be7f8823ea0fb3ccebba40f9d8e08be88
Huiling Wu <huwu@ucdavis.edu>
2019-01-10 18:15:34 -0800

    connected jawsdb

--- app/db/seeds.sql
+++ app/db/seeds.sql
@@ -1,3 +1,7 @@
 USE friends_db;
 
-INSERT INTO friends (name, photo_link, answer_1, answer_2, answer_3, answer_4, answer_5, answer_6, answer_7, answer_8, answer_9, answer_10) VALUES ("Bob", "https://dcmetrotheaterarts.com/wp-content/uploads/2017/12/SpongeBob_SquarePantscopy0.png", 1, 2, 3, 4, 2, 4, 3, 2, 5, 5);
+INSERT INTO friends (name, photo_link, answer_1, answer_2, answer_3, answer_4, answer_5, answer_6, answer_7, answer_8, answer_9, answer_10) VALUES 
+("Bob", "https://dcmetrotheaterarts.com/wp-content/uploads/2017/12/SpongeBob_SquarePantscopy0.png", 1, 2, 3, 4, 2, 4, 3, 2, 5, 5),
+("Harry Potter", "https://images.ctfassets.net/bxd3o8b291gf/3SQ3X2km8wkQIsQWa02yOY/25f258f21bdbe5f552a4419bb775f4f0/HarryPotter_WB_F4_HarryPotterMidshot_Promo_080615_Port.jpg?w=1200", 3, 4, 5, 2, 1, 1, 1, 2, 3, 4),
+("Hermione Granger", "https://vignette.wikia.nocookie.net/harrypotter/images/4/41/Hermionedhface.jpg/revision/latest?cb=20161221044857", 2, 2, 3, 1, 1, 4, 5, 5, 1, 2),
+("Ronald Weasley", "https://vignette.wikia.nocookie.net/harrypotter/images/8/85/Ron_Weasley.jpg/revision/latest/scale-to-width-down/350?cb=20150828153318", 3, 3, 3, 3, 4, 1, 2, 1, 5, 3);
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,16 +4,23 @@ require("dotenv").config();
 
 var mysql_pw = process.env.MYSQL;
 
-var connection = mysql.createConnection({
-    host: "localhost",
+var connection;
 
-    port: 3306,
-
-    user: "root",
-
-    password: mysql_pw,
-    database: "friends_db"
-});
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+    
+        port: 3306,
+    
+        user: "root",
+    
+        password: mysql_pw,
+        database: "friends_db"
+    });    
+}
 
 connection.connect(function(err) {
     if (err) {
